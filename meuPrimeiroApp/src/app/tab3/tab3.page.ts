a0573eda85e12da1aef924fa1aee30c77f40a548
Raniel Santos <ranielsnts@hotmail.com>
2020-06-24 01:42:37 +0000

    atualizando

--- meuPrimeiroApp/src/app/tab3/tab3.page.ts
+++ meuPrimeiroApp/src/app/tab3/tab3.page.ts
@@ -6,7 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['tab3.page.scss']
 })
 export class Tab3Page {
+   
+    private altura:number;
+    private centimetros:number;
+    private resultado:number;
+    private sexo:string;
+    
+    
+   constructor() {}
 
-  constructor() {}
+   pesoIdeal(){
+       this.centimetros = this.altura*100;
+       
+       if(this.sexo == "m")
+       {
+            this.resultado = (this.centimetros-100)-((this.centimetros-150)/4);
+       }else if(this.sexo == "f")
+       {
+            this.resultado = (this.centimetros-100)-((this.centimetros-150)/2);
+       }
+   }
 
+    exibirResultado(): number{
+        return this.resultado
+    }
 }
